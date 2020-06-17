cf81326b7e3b8e339e8bc16e899c65d6b90071f1
Christian2019 <37154877+Christian2019@users.noreply.github.com>
2020-06-17 00:26:51 -0300

    Criando Centroids Aleatorios

--- src/main/Agrupamento.java
+++ src/main/Agrupamento.java
@@ -3,11 +3,37 @@ package main;
 import java.util.ArrayList;
 
 public class Agrupamento {
-	public static String path="iris.txt";
+	public static String path = "iris.txt";
 	public static ArrayList<Petala> petalas;
+	static int k = 3;
+	static ArrayList<Petala> centroids;
+
 	public static void Agrupar() {
 		petalas = new ArrayList<Petala>();
+		centroids= new ArrayList<Petala>();
 		Save_Game.load(path);
-		System.out.println(petalas);
+		// System.out.println(petalas);
+		for (int i = 0; i < k; i++) {
+
+			boolean repetido = true;
+			Petala centroid = null;
+			while (repetido) {
+				repetido = false;
+				int random = Game.rand.nextInt(petalas.size());
+				centroid = petalas.get(random);
+
+				for (int j = 0; j < centroids.size(); j++) {
+					if ((centroid.petal_length == centroids.get(j).petal_length)
+							&& (centroid.petal_width == centroids.get(j).petal_width)) {
+						repetido = true;
+						break;
+					}
+				}
+			
+			}
+			centroids.add(centroid);
+			
+		}
+		System.out.println(centroids);
 	}
 }
